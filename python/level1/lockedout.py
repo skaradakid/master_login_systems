0fd09fc5ad70afc841d4eee2fff8e552e53e21c5
karabo <kaphash024jhb@student.wethinkcode.co.za>
2025-06-30 23:41:32 +0200

    implemented registration function

--- python/level1/lockedout.py
+++ python/level1/lockedout.py
@@ -21,5 +21,41 @@ def login():
     print("coming soon!")
 
 def signup():
-    print("coming soon!")
-main()
+    while True:
+        name = input("name: ")
+        with open("python/level1/users.txt", "r") as file:
+            open_file = file.readlines()
+            new_list = []
+            for x in open_file:
+                new_list.append(x.strip().split(" "))
+            list_of_names= []
+            for x in new_list:
+                list_of_names.append(x[0])
+        if name in list_of_names:
+            print("username already exist")
+        else:
+            break
+                
+    with open("python/level1/users.txt", "a") as file2:
+        while True:
+            password = input("password: ")
+            password2 = input("confirm password: ")
+            if password != password2:
+                print("passwords do not match")
+            else:
+                print("passwords matched!")
+                break
+        file2.write(f"\n{name} {password}")
+        print("signup complete")
+    
+    print("would you like to login? y/n")
+    while True:
+        answer = input("=> ")
+        if answer != "y" and answer != "n":
+            print("choose from the given options y/n")
+        elif answer == "y":
+            login()
+        elif answer == "n":
+            print("good bye!")
+        break    
+signup()
